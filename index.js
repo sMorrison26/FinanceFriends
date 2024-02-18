70c94b0a31bf2c83fadaaa21da43aa4b7fbcb971
EdenBendheim <edenbendheim@gmail.com>
2024-02-18 00:40:46 -0500

    budget on map

--- index.js
+++ index.js
@@ -55,11 +55,25 @@ resetButton.onclick = function () {
         bearing: 125
     });
 };
+
 const tasksdiv = document.createElement('div');
 tasksdiv.id = 'tasks';
 tasksdiv.innerHTML = `<div id="tasksList"></div>`;
-map.getCanvas().parentNode.appendChild(tasksdiv);
 
+const budgetdiv = document.createElement('div');
+budgetdiv.id = 'budget';
+budgetdiv.innerHTML = `<div>
+<p>My Budget:           
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="listbullet">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+  </svg>   
+</p>
+<p>My Transactions:</p>
+<p>Remaining Balance: </p>
+</div>`;
+
+map.getCanvas().parentNode.appendChild(tasksdiv);
+map.getCanvas().parentNode.appendChild(budgetdiv);
 map.getCanvas().parentNode.appendChild(resetButton);
 
 function loadMarkers() {
