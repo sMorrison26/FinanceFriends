355b51c5faf573dc753f03858ee76194c2232bfa
sMorrison26 <morris10@rpi.edu>
2024-02-17 19:48:28 -0500

    proj

--- financefriends/src/components/navbar.component.tsx
+++ financefriends/src/components/navbar.component.tsx
@@ -1,3 +1,14 @@
+import Navbarlink from "@/components/navbarlink.component";
+
 export default function Navbar(){
-  return(<></>);
+  return(
+    <nav className="flex justify-between px-4 py-8 font-medium text-white bg-black">
+      <p className="text-3xl">Finance Friends: Explore Troy</p>
+      <div className="flex justify-between w-1/3">
+        <Navbarlink href="./instruction" text="Instructions"></Navbarlink>
+        <Navbarlink href="./resources" text="Resources"></Navbarlink>
+        <Navbarlink href="./about" text="About Us"></Navbarlink>
+      </div>
+    </nav>
+  );
 }
