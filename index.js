e23a605df335aa889cb6b90f7c16d0f7808ff128
sMorrison26 <morris10@rpi.edu>
2024-02-17 23:44:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,13 @@ function getBusinesses(){
         url: './businesses.geojson',
         content: 'json',
         success: function(data) {
-            let output = `<p>My Tasks: </p>`;
+            let output = `
+            <p style="display:flex; justify-content:space-between;align-items:center;">
+                My Tasks: 
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="listbullet">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                </svg>
+            </p>`;
             output += `
             <table id="tasksTable">
                 <tr style="text-align:left">
