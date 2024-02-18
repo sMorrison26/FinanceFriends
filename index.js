c403c33a0364352a8d9407e5d915dd8814831ff7
EdenBendheim <edenbendheim@gmail.com>
2024-02-17 20:52:52 -0500

    MAPPA OPPA CHOPPAAAA
    MAPPPA CHO{AA OPPA

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGVucnlyb2JiIiwiYSI6ImNsc3E5cWZwbTB6MWQybm51ZWhnNXZqdGYifQ.VP-6WVFeERn_zB1sN8PZdA';
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/examples/clg45vm7400c501pfubolb0xz',
-      center: [-87.661557, 41.893748],
-      zoom: 10.7
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/henryrobb/clsqtqx3i00fd01o4b21p132d',
+    center: [-73.691482, 42.732983],
+    zoom: 16,
+    maxZoom: 25,
+    pitch: 65,
+    bearing: 125
+});
+
+map.on('click', (event) => {
+    const features = map.queryRenderedFeatures(event.point, {
+        layers: ['businesses-test']
     });
-
-    map.on('click', (event) => {
-      const features = map.queryRenderedFeatures(event.point, {
-        layers: ['chicago-parks']
-      });
-      if (!features.length) {
+    if (!features.length) {
         return;
-      }
-      const feature = features[0];
+    }
+    const feature = features[0];
 
-      const popup = new mapboxgl.Popup({ offset: [0, -15] })
+    const popup = new mapboxgl.Popup({ offset: [0, -15] })
         .setLngLat(feature.geometry.coordinates)
         .setHTML(
-          `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p><button`
+            `<h3>${feature.properties.name}</h3><p>${feature.properties.description}</p><a href="${feature.properties.website}">Swag</a>`
 
         )
         .addTo(map);
-      
 
-    });
-    function addMarker() {
-      const popup = new mapboxgl.Popup({ offset: [0, -15] })
+
+});
+function addMarker() {
+    const popup = new mapboxgl.Popup({ offset: [0, -15] })
         .setLngLat([-87.637596, 41.940403])
         .setHTML(
-          `<h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p>`
+            `<h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p><h3>Lincoln Park</h3><p>A northside park that is home to the Lincoln Park Zoo</p>`
 
         )
         .addTo(map);
-    }
+}
